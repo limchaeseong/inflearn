f2c12a3f77caab2e240afacb8ab149998ab17cfb
limchaeseong <lcs20043@naver.com>
2023-09-12 09:13:04 +0900

    처음만난 리액트 - CH15.Styling

---
+++ react/mini-blog/src/component/list/CommentList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import CommentListItem from "./CommentListItem";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  
+  & > * {
+    :not(:last-child){
+      margin-bottom: 16px;
+    }
+  }
+`;
--- react/my-react/src/index.js
+++ react/my-react/src/index.js
@@ -4,72 +4,29 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// import Library from './chapter_03/Library';
-// import Clock from './chapter_04/Clock';
-// import CommentList from "./chapter_05/CommentList";
-// import NotificationList from "./chapter_06/NotificationList";
-// import Accommodate from "./chapter_07/Accommodate";
-// import ConfirmButton from "./chapter_08/ConfirmButton";
-// import LandingPage from "./chapter_09/LandingPage";
-// import AttendanceBook from "./chapter_10/AttendanceBook";
-import SignUp from "./chapter_11/SignUp";
+// import Item from './chapter_03/Library';
+// import Item from './chapter_04/Clock';
+// import Item from "./chapter_05/CommentList";
+// import Item from "./chapter_06/NotificationList";
+// import Item from "./chapter_07/Accommodate";
+// import Item from "./chapter_08/ConfirmButton";
+// import Item from "./chapter_09/LandingPage";
+// import Item from "./chapter_10/AttendanceBook";
+// import Item from "./chapter_11/SignUp";
+import Item from "./chapter_15/Block";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// ====chapter11=====
-root.render(
-    <SignUp />
-);
-
-// ====chapter10=====
-// root.render(
-//     <AttendanceBook />
-// );
-
-// ====chapter9=====
-// root.render(
-//     <LandingPage />
-// );
-
-// ====chapter8=====
-// root.render(
-//     <ConfirmButton />
-// );
-
-// ====chapter7=====
-// root.render(
-//   <Accommodate />
-// );
-
-// ====chapter6=====
-// root.render(
-//   // <React.StrictMode>
-//       <NotificationList />
-//   // </React.StrictMode>
-// );
-
-// ====chapter5=====
-// root.render(
-//   <React.StrictMode>
-//       <CommentList />
-//   </React.StrictMode>
-// );
-
 // ====chapter4=====
 // setInterval(()=>{
-//     root.render(
-//         <React.StrictMode>
-//             <Clock />
-//         </React.StrictMode>
-//     )
-// }, 1000);
 
-// ====chapter3=====
-// root.render(
-//   <React.StrictMode>
-//       <Library />
-//   </React.StrictMode>
-// );
+// ====chapter3 ~ 16=====
+    root.render(
+        <Item />
+    );
+
+// ====chapter4=====
+// }, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
